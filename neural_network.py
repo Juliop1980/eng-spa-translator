c868b30f09b8c51a37f1303344acc21f2a147ed8
Tettoni, Anne Laure Olga <tettoni@campus.tu-berlin.de>
2022-07-29 16:58:20 +0200

    cleanup

--- neural_network.py
+++ neural_network.py
@@ -1,20 +1,20 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 import collections
-#import helper
+# import helper
 import numpy as np
 from keras.preprocessing.text import Tokenizer
 from keras_preprocessing.sequence import pad_sequences
 from keras.models import Model, Sequential
 from keras.layers import GRU, Input, Dense, TimeDistributed, Activation, RepeatVector, Bidirectional, Dropout, LSTM
-from tensorflow.keras.layers import Embedding
+# from tensorflow.keras.layers import Embedding
 from keras.optimizers import Adam
 from keras.losses import sparse_categorical_crossentropy
-#from nltk.tokenize import tokenize
+# from nltk.tokenize import tokenize
 
 import tensorflow as tf
-print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
 
+print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
 
 
 def tokenize(x):
@@ -26,7 +26,7 @@ def tokenize(x):
     # TODO: Implement
     x_tk = Tokenizer()
     x_tk.fit_on_texts(x)
- 
+
     return x_tk.texts_to_sequences(x), x_tk
 
 
@@ -43,23 +43,21 @@ def pad(x, length=None):
     return pad_sequences(x, maxlen=length, padding='post', truncating='post')
 
 
-
-#tests.test_tokenize(tokenize)
+# tests.test_tokenize(tokenize)
 # read csv input data
 data = pd.read_csv('mixedtranslation.csv')
 english_sentences = data['english_sentences'].fillna("")
 spanish_sentences = data['spanish_sentences'].fillna("")
-#print(data)
-#train, test = train_test_split(df, test_size=0.2)
-#X_train, X_test, y_train, y_test = train_test_split(english_sentences, spanish_sentences, test_size=0.2, random_state=1)
+# print(data)
+# train, test = train_test_split(df, test_size=0.2)
+# X_train, X_test, y_train, y_test = train_test_split(english_sentences, spanish_sentences, test_size=0.2, random_state=1)
 
 
+# X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.25, random_state=1) # 0.25 x 0.8 = 0.2
 
-#X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.25, random_state=1) # 0.25 x 0.8 = 0.2
-
-#print(english_sentences)
-#print("------------")
-#print(spanish_sentences)
+# print(english_sentences)
+# print("------------")
+# print(spanish_sentences)
 
 english_words_counter = collections.Counter([word for sentence in english_sentences for word in sentence.split()])
 spanish_words_counter = collections.Counter([word for sentence in spanish_sentences for word in sentence.split()])
@@ -74,6 +72,8 @@ print('{} unique French words.'.format(len(spanish_words_counter)))
 print('10 Most common words in the French dataset:')
 print('"' + '" "'.join(list(zip(*spanish_words_counter.most_common(10)))[0]) + '"')
  """
+
+
 def preprocess(x, y):
     """
     Preprocess x and y
@@ -83,24 +83,26 @@ def preprocess(x, y):
     """
     preprocess_x, x_tk = tokenize(x)
     preprocess_y, y_tk = tokenize(y)
- 
+
     preprocess_x = pad(preprocess_x)
     preprocess_y = pad(preprocess_y)
- 
+
     # Keras's sparse_categorical_crossentropy function requires the labels to be in 3 dimensions
     preprocess_y = preprocess_y.reshape(*preprocess_y.shape, 1)
- 
+
     return preprocess_x, preprocess_y, x_tk, y_tk
 
-preproc_english_sentences, preproc_spanish_sentences, english_tokenizer, spanish_tokenizer =preprocess(english_sentences, spanish_sentences)
+
+preproc_english_sentences, preproc_spanish_sentences, english_tokenizer, spanish_tokenizer = preprocess(
+    english_sentences, spanish_sentences)
 print(spanish_tokenizer)
 print("----------------------------------------------------------------------")
-     
+
 max_english_sequence_length = preproc_english_sentences.shape[1]
 max_spanish_sequence_length = preproc_spanish_sentences.shape[1]
 english_vocab_size = len(english_tokenizer.word_index)
 spanish_vocab_size = len(spanish_tokenizer.word_index)
- 
+
 """ print('Data Preprocessed')
 print("Max English sentence length:", max_english_sequence_length)
 print("Max Spanishsentence length:", max_spanish_sequence_length)
@@ -117,7 +119,7 @@ def logits_to_text(logits, tokenizer):
     """
     index_to_words = {id: word for word, id in tokenizer.word_index.items()}
     index_to_words[0] = '<PAD>'
- 
+
     return ' '.join([index_to_words[prediction] for prediction in np.argmax(logits, 1)])
 
 
@@ -135,26 +137,26 @@ def simple_model(input_shape, output_sequence_length, english_vocab_size, french
     model = Sequential()
     model.add(GRU(128, input_shape=input_shape[1:], return_sequences=True))
     model.add(Dropout(0.5))
-    #model.add(GRU(128, return_sequences=True))
-    #model.add(Dropout(0.5))
+    # model.add(GRU(128, return_sequences=True))
+    # model.add(Dropout(0.5))
     model.add(TimeDistributed(Dense(256, activation='relu')))
-    #model.add(Dropout(0.5))
-    model.add(TimeDistributed(Dense(french_vocab_size, activation='softmax'))) 
-     
-     
+    # model.add(Dropout(0.5))
+    model.add(TimeDistributed(Dense(french_vocab_size, activation='softmax')))
+
     model.compile(loss=sparse_categorical_crossentropy,
                   optimizer=Adam(learning_rate),
                   metrics=['accuracy'])
     return model
 
-#tests.test_simple_model(simple_model)
- 
+
+# tests.test_simple_model(simple_model)
+
 # Reshaping the input to work with a basic RNN
 tmp_x = pad(preproc_english_sentences, max_spanish_sequence_length)
 tmp_x = tmp_x.reshape((-1, preproc_spanish_sentences.shape[-2], 1))
- 
-#print((tmp_x[:1])[0])
- 
+
+# print((tmp_x[:1])[0])
+
 # Train the neural network
 simple_rnn_model = simple_model(
     tmp_x.shape,
@@ -162,7 +164,7 @@ simple_rnn_model = simple_model(
     english_vocab_size,
     spanish_vocab_size)
 simple_rnn_model.fit(tmp_x, preproc_spanish_sentences, batch_size=10, epochs=1, validation_split=0.2)
- 
+
 # Print prediction(s)
 print(logits_to_text(simple_rnn_model.predict(tmp_x[:1])[0], spanish_tokenizer))
 """ for sample_i, (sent, token_sent) in enumerate(zip(text_sentences, text_tokenized)):
--- pre-processing.py
+++ pre-processing.py
@@ -3,14 +3,11 @@ import string
 import num2words
 
 from typing import List
-from lxml import etree
 import csv
-#from nltk.corpus import stopwords
 
-
-#tree = etree.parse('es-en/europarl-v7.es-en.en')
-#notags = etree.tostring(tree, encoding='utf8', method='text')
-#print(notags)
+# tree = etree.parse('es-en/europarl-v7.es-en.en')
+# notags = etree.tostring(tree, encoding='utf8', method='text')
+# print(notags)
 filename_en = "es-en/europarl-v7.es-en.en"
 english_sentences = []
 with open(filename_en, 'r', encoding='UTF-8') as file:
@@ -24,75 +21,47 @@ with open(filename_es, 'r', encoding='UTF-8') as file:
         spanish_sentences.append(line.rstrip())
 
 nb_sentences_en = len(english_sentences)
-# nb_sentences_es = len(spanish_sentences)
-# print("nb en sentences : " + str(nb_sentences_en))
-# print("nb es sentences : " + str(nb_sentences_es))
-
+nb_sentences_es = len(spanish_sentences)
+print("nb en sentences : " + str(nb_sentences_en))
+print("nb es sentences : " + str(nb_sentences_es))
+print("nb tot sentences : " + str(nb_sentences_en + nb_sentences_es))
 indexes = range(nb_sentences_en)
 # to randomly select 10 percent of the data
 indexes_ten_percent = np.random.choice(indexes, int(nb_sentences_en / 10))
-indexes_ten_percent = np.random.choice(indexes, int(nb_sentences_en / 100))
+# indexes_ten_percent = np.random.choice(indexes, int(nb_sentences_en / 100))
 english_sentences = [english_sentences[i] for i in indexes_ten_percent]
 spanish_sentences = [spanish_sentences[i] for i in indexes_ten_percent]
-
-"""for i in range(100):
-    print(english_sentences[i])
-    print(spanish_sentences[i])"""
 
 # pre-processing
 english_sentences_preprocessed = []
 spanish_sentences_preprocessed = []
 
+
 def pre_process_list(list_sentences: list[str], list_preprocessed: list[str], language: str) -> list[str]:
     for curr_sentence in list_sentences:
-        if curr_sentence != '':
-            # lowercase
-            curr_sentence = curr_sentence.lower()
-            # remove punctuation
-            curr_sentence = curr_sentence.translate(str.maketrans('', '', string.punctuation))
-            # change numbers into word equivalents and remove white spaces
-            curr_sentence = ' '.join([num2words.num2words(word, lang=language) if word.isdigit() else word
-                                      for word in curr_sentence.split()])
-            list_preprocessed.append(curr_sentence)
-    return list_preprocessed
-
-
-english_sentences_preprocessed = pre_process_list(english_sentences, english_sentences_preprocessed, "en")
-spanish_sentences_preprocessed = pre_process_list(spanish_sentences, spanish_sentences_preprocessed, "es")
-
-for i in range(100):
-    print(english_sentences_preprocessed[i])
-    print(spanish_sentences_preprocessed[i])
-
-np.savetxt("pre-processed_data_en.csv", english_sentences_preprocessed, delimiter=" ", fmt='%s')
-np.savetxt("pre-processed_data_es.csv", spanish_sentences_preprocessed, delimiter=" ", fmt='%s')
-def pre_process_list(list_sentences: List[str], list_preprocessed: List[str], language: str) -> List[str]:
-    #curr_sentence = curr_sentence.replace("<", "").replace(">", "")
-    for curr_sentence in list_sentences:
-        #if curr_sentence != '':
-            # lowercase
+        # if curr_sentence != '':
+        # lowercase
         curr_sentence = curr_sentence.lower()
         # remove punctuation
         curr_sentence = curr_sentence.translate(str.maketrans('', '', string.punctuation))
         # change numbers into word equivalents and remove white spaces
         curr_sentence = ' '.join([num2words.num2words(word, lang=language) if word.isdigit() else word
-                                    for word in curr_sentence.split()])
+                                  for word in curr_sentence.split()])
         list_preprocessed.append(curr_sentence)
     return list_preprocessed
 
-#print(len(english_sentences))
-#print(len(spanish_sentences))
+
+print(len(english_sentences))
+print(len(spanish_sentences))
 english_sentences_preprocessed = pre_process_list(english_sentences, english_sentences_preprocessed, "en")
 spanish_sentences_preprocessed = pre_process_list(spanish_sentences, spanish_sentences_preprocessed, "es")
 
-""" for i in range(100):
+"""for i in range(10):
     print(english_sentences_preprocessed[i])
-    print(spanish_sentences_preprocessed[i]) """
+    print(spanish_sentences_preprocessed[i])
 
-np.savetxt("pre-processed_data_en.csv", english_sentences_preprocessed, delimiter=" ", fmt='%s')
-np.savetxt("pre-processed_data_es.csv", spanish_sentences_preprocessed, delimiter=" ", fmt='%s')
 print(len(english_sentences_preprocessed))
-print(len(spanish_sentences_preprocessed))
+print(len(spanish_sentences_preprocessed))"""
 """ 
 headerList = ['english_sentences', 'spanish_stences']
 with open('mixedtranslation.csv', 'w') as f:
@@ -105,5 +74,5 @@ with open('mixedtranslation.csv', 'w') as csvfile:
 
     writer.writeheader()
     for i in range(len(english_sentences_preprocessed)):
-        writer.writerow({'english_sentences': english_sentences_preprocessed[i], 'spanish_sentences': spanish_sentences_preprocessed[i]})
-
+        writer.writerow({'english_sentences': english_sentences_preprocessed[i],
+                         'spanish_sentences': spanish_sentences_preprocessed[i]})
